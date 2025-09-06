0c84ee209f361ae344c388449f18f9e0a71a6f81
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-09-07 01:52:13 +0530

    NewTailwind1

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 echo "BUILD START"
 
-# Install dependencies
 python3 -m pip install -r requirements.txt
-
-# Run migrations
 python3 manage.py migrate --noinput
-
-# Collect all static files (admin CSS/JS too)
 python3 manage.py collectstatic --noinput --clear
+
+# 👇 Add this to debug what files are in staticfiles
+echo "---- STATICFILES CONTENT ----"
+ls -R staticfiles
 
 echo "BUILD END"
