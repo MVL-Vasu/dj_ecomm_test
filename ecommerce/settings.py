ac7716bb99941b5f7c3504a3e7ff3377c19d03e3
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-09-07 01:21:14 +0530

    Fix Tailwind static setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -184,8 +184,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'theme/static'),
+    BASE_DIR / "theme" / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
