fd9e8d44540f4fbcbf022564dfcb2970c7efe9a2
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-09-07 01:11:17 +0530

    Tailwind5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -184,10 +184,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / "theme" / "static",
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'theme/static'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
