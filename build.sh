eaecf729dcd620c871c6d4aaa915962744fa50d0
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-09-07 00:19:15 +0530

    Commit-4

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ echo "BUILD START"
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput
 echo "BUILD END"
